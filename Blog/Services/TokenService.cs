6beb9d55df984a988db8dddb048040a78867232e
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-01-11 18:03:27 -0300

    feat: Define Claims hard-coded exemplificando

--- Blog/Services/TokenService.cs
+++ Blog/Services/TokenService.cs
@@ -1,5 +1,6 @@
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 namespace Blog.Services;
@@ -12,6 +13,11 @@ public class TokenService
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.Name, "renebentes"),
+                new (ClaimTypes.Role, "admin")
+            }),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
