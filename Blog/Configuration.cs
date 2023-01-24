ef277f3be6869ae831cbc233ae13fbaae29d757a
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-01-23 21:45:04 -0300

    feat: Define atributo para autorização via API Key

--- Blog/Configuration.cs
+++ Blog/Configuration.cs
@@ -2,5 +2,7 @@ namespace Blog;
 
 public static class Configuration
 {
-    public static string JwtKey { get; set; } = "bW9LTrd6TtWqXVUyX1oXRQ==";
+    public const string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+    public const string ApiKeyName = "api_key";
+    public const string JwtKey = "bW9LTrd6TtWqXVUyX1oXRQ==";
 }
