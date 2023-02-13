667563945beb8fa28697b05877f4d5b108db30d1
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-02-13 15:35:42 -0300

    feat: Obtém posts por categoria

--- Blog/Controllers/PostsController.cs
+++ Blog/Controllers/PostsController.cs
@@ -52,6 +52,54 @@ public class PostsController : ControllerBase
         }
     }
 
+    [HttpGet("category/{category}")]
+    public async Task<IActionResult> ListPostsByCategory([FromServices] BlogDataContext context,
+                                                         [FromRoute] string category,
+                                                         [FromQuery] int page = 0,
+                                                         [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var total = await context
+                .Posts
+                .AsNoTracking()
+                .CountAsync();
+
+            var posts = await context
+                .Posts
+                .AsNoTracking()
+                .Include(p => p.Category)
+                .Include(p => p.Author)
+                .Where(p => p.Slug == category)
+                .Select(p => new ListPostViewModel
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Slug = p.Slug,
+                    LastUpdateDate = p.LastUpdateDate,
+                    Category = p.Category.Title,
+                    Author = $"{p.Author.Name} ({p.Author.Email})"
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(p => p.LastUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch (Exception e)
+        {
+            // TODO: Log exception
+            return StatusCode(500, new ResultViewModel<List<Post>>("Erro interno do servidor."));
+        }
+    }
+
     [HttpGet("{id:int}")]
     public async Task<IActionResult> ShowPost([FromServices] BlogDataContext context, [FromRoute] int id)
     {
