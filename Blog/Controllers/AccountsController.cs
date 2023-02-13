4b29b6e5a373555e42ccfacbe01ad0b6d7b0c87b
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-02-13 14:15:12 -0300

    feat: Implementa upload de imagem

--- Blog/Controllers/AccountsController.cs
+++ Blog/Controllers/AccountsController.cs
@@ -1,6 +1,8 @@
 using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers;
 
@@ -85,5 +87,45 @@ public class AccountsController : ControllerBase
             // TODO: Log exception
             return StatusCode(500, new ResultViewModel<Category>("Erro interno do servidor ao registrar um usuário."));
         }
+    }
+
+    [Authorize]
+    [HttpPost("upload-image")]
+    public async Task<IActionResult> UploadImage([FromBody] UploadImageViewModel model, [FromServices] BlogDataContext context)
+    {
+        try
+        {
+            var fileName = $"{Guid.NewGuid()}.jpg";
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+            var bytes = Convert.FromBase64String(data);
+
+            await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+
+            var user = await context
+                .Users
+                .FirstOrDefaultAsync(u => u.Email == User.Identity.Name);
+
+            if (user is null)
+            {
+                return NotFound(new ResultViewModel<string>("Usuário não encontrado!"));
+            }
+
+            user.Image = $"http://localhost:0000/images/{fileName}";
+
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateException)
+        {
+            // TODO: Log exception
+            return StatusCode(500, new ResultViewModel<Category>("Houve um erro de banco de dados ao salvar imagem do usuário."));
+        }
+        catch (Exception)
+        {
+            // TODO: Log exception
+            return StatusCode(500, new ResultViewModel<Category>("Erro interno do servidor ao salvar a imagem do usuário."));
+        }
+
+        return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
     }
 }
