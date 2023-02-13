2f12fd377f878c8ca4c549bc4179b3cd8689744d
Rene Bentes Pinto <renebentes@yahoo.com.br>
2023-02-13 14:42:04 -0300

    feat(json): Ignora referência circular e campos com nulos

--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,7 +1,16 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddJwtAuthentication();
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+    });
+
 builder.AddServices();
 
 var app = builder.Build();
